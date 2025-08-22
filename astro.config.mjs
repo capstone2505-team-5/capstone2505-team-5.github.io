3338fad15a42ce12745dab9abd2cbd24b46faf98
Joshua Cutts <josh.cutts1@gmail.com>
2025-08-22 15:33:56 -0700

    add diagram

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 export default defineConfig({
   site: 'https://capstone2505-team-5.github.io/',
+  integrations: [react()],
 });
 // https://astro.build/config
